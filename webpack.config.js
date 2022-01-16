ec21e06d366fdd39086c8ba11fa04b3433a21318
Andy <cuelloandres.js@gmail.com>
2022-01-16 12:03:49 -0300

    fix: deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require("path");
 
-mode: "development",
-    (module.exports = {
-        entry: "./src/index.js",
-        module: {
-            rules: [
-                {
-                    test: /\.ts?x$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                    },
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.ts?x$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
                 },
-            ],
-        },
-        resolve: {
-            extensions: [".tsx", ".ts", ".js"],
-        },
-    });
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+};
