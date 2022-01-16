b8b92f552b3c45fddb1a4ca2694903de09ad34f7
Andy <cuelloandres.js@gmail.com>
2022-01-16 11:49:44 -0300

    feat: skip build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
 const path = require("path");
 
-module.exports = {
-    mode: "development",
-
-    entry: "./src/index.js",
-    module: {
-        rules: [
-            {
-                test: /\.ts?x$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
+mode: "development",
+    (module.exports = {
+        entry: "./src/index.js",
+        module: {
+            rules: [
+                {
+                    test: /\.ts?x$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                    },
                 },
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-};
+            ],
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+    });
