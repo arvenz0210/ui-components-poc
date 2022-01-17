a59ee4ba6748d627511c6327aa0d8381b33ef53e
Andy <cuelloandres.js@gmail.com>
2022-01-17 08:37:54 -0300

    feat: postCSS setup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.sass";
+import "./index.css";
 
 interface ButtonProps {
     text: string;
--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.sass";
+import "./index.css";
 
 interface TagProps {
     text: string;
